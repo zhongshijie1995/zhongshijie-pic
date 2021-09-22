a52602905674a2a26c81d4e9f6159ce96d8aa4a5
zhongshijie <zhongshijie1995@outlook.com>
2021-09-22 22:05:54 +0800

    20211005

---
+++ auto-commit.sh
@@ -0,0 +1,6 @@
+echo ' ***** mirrors-pic Auto Commit ***** *'
+git add .
+git commit -m 'AutoCommit'
+echo ' ***** mirrors-pic Auto Push ***** *'
+git push
+echo ' ***** mirrors-pic Auto Finish ***** '
---
+++ readme.md
@@ -0,0 +1,9 @@
+# mirrors-pic
+个人镜像-图床
+
+[仓库地址Page页](https://gitee.com/zhongshijie/mirrors-pic/pages)
+
+## 子目录描述
+子目录|描述|备注
+--|--|--
+/img|图床| 以时间戳命名的图片
