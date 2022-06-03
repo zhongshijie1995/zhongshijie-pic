dcaaf2178981e18d67c9df9e392991ea1d4db588
钟世杰 <zhongshijie1995@outlook.com>
2022-06-03 23:07:58 +0800

    AutoCommit

--- auto-commit.sh
+++
@@ -1,7 +0,0 @@
-echo ' ***** zhongshijie-pic Auto Commit ***** *'
-git pull
-git add .
-git commit -m 'AutoCommit'
-echo ' ***** zhongshijie-pic Auto Push ***** *'
-git push
-echo ' ***** zhongshijie-pic Auto Finish ***** '
